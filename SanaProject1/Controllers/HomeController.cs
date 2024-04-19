768b7907a6521fb49826ffd338fa8c30c48d5ab1
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2024-04-19 16:09:03 +0300

    Completed the task

---
+++ SanaProject1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using SanaProject1.Models;
+using System.Diagnostics;
+
+namespace SanaProject1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Check(UserModel user)
+        {
+            ViewBag.Name = user.Name;
+			if (ModelState.IsValid)
+				return View("Check");
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SanaProject1/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Transactions;
+
+namespace SanaProject1.Models
+{
+    public class UserModel
+    {
+        [Display(Name = "Enter yor name")]
+        [Required(ErrorMessage = "You didn't enter a name")]
+        public string Name { get; set; }
+    }
+}
